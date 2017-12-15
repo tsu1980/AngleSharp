dcab3719e93ccb1590f266f445682d5d9c7adc72
tsu1980 <tsu1980@gmail.com>
2017-12-15 13:57:53 +0900

    [#153204919]AnguleSharpを.NET Coreに対応する (https://www.pivotaltracker.com/story/show/153204919)

--- AngleSharp/Properties/AssemblyInfo.cs
+++ AngleSharp/Properties/AssemblyInfo.cs
@@ -1,10 +1,10 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("AngleSharp")]
 [assembly: AssemblyDescription("AngleSharp is the ultimate angle brackets parser library. It parses HTML5, MathML, SVG, XML and CSS to construct a DOM based on the official W3C specification.")]
-[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("AngleVisions")]
 [assembly: AssemblyProduct("AngleSharp")]
 [assembly: AssemblyCopyright("Copyright © Florian Rappl et al. 2013-2015")]
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: InternalsVisibleToAttribute("AngleSharp.Core.Tests")]
-[assembly: AssemblyVersion("0.8.9.*")]
+//[assembly: AssemblyVersion("0.8.9.*")]
 [assembly: AssemblyFileVersion("0.8.9")]
